b1121b723ec38c413fbec645fe2661026707b985
ssekiziyivu grace <149536222+Grace444-arch@users.noreply.github.com>
2026-07-02 11:38:58 +0300

    fix: move modules to correct DailyReportSystem-BACKEND path

--- notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from notifications.views import SendTestEmailView
-
-app_name = "notifications"
-
-urlpatterns = [
-    path("test-email/", SendTestEmailView.as_view(), name="test-email"),
-]
